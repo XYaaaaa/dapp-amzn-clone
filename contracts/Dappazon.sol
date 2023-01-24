7a6de9a3bccc1cbb338c3ce70e06ebc6d72eae85
Sean Xiao <441829909@qq.com>
2023-01-24 01:01:36 -0500

    update

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -3,5 +3,26 @@ pragma solidity ^0.8.9;
 
 contract Dappazon {
 
-    string public name = "Dappazon";
+    string public name;
+    address public owner;
+
+    constructor(){
+        name = "Dappazon";
+        owner = msg.sender;
+    }
+
+    //List products
+    function list(
+    uint256 _id, 
+    string memory _name, 
+    string memory _category,
+    string memory _image,
+    uint256 _cost,
+    uint256 _rating,
+    uint256 _stock  ) public {
+
+    }
+    //Buy products
+
+    //Withdraw funds
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -8,12 +8,25 @@ const tokens = (n) => {
 describe("Dappazon", () => {
 
   let dappazon
+  let deployer, buyer
+
   beforeEach(async () => {
+
+    //setup accounts
+    [deployer, buyer] = await ethers.getSigners()
+    
+
+    //Deploy contract
     const Dappazon = await ethers.getContractFactory("Dappazon")
     dappazon = await Dappazon.deploy()
   })
 
   describe("Deployment", () =>{
+
+    it('Sets the owner', async() => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+
     it('has a name', async ()=>{
       const name = await dappazon.name()
       expect(name).to.equal("Dappazon")
